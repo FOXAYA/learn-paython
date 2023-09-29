4b1e4708ebe110d11c53147835eaf6a0a808639a
FOXAYA <omniasaged111@gmail.com>
2023-09-30 02:06:45 +0300

    paython vars

---
+++ new_lang.py
@@ -0,0 +1,23 @@
+#------------------task 01----------------------------------->
+#my first project with paython
+#This is multiply lines 
+#contains variables ,comments,concatenation,some basics
+#------------------------------------------------------>
+#task02
+name = "omnia" #normal name
+myAge =  30     #camel Case
+my_Counrtry = "Egypt" #snake case
+print(name , myAge , my_Counrtry)
+#task03
+name ="osama" 
+age = "38"
+country = "Egypt"
+print("\"Name : " + name + "\"")
+print("\"Age : " + age + "\"")
+print("\"Country : " + country + "\"") 
+#task 04 
+print("Hello" + "," +" "+"My"+ " " + "Name is " + name + " "+ "And" + " " + "I am" +" "+ age + " " + "years old" + " " + "and" + " " + "I live in " + " " + country )
+# task 05
+print(type(name))
+print(type(age))
+print(type(country))
