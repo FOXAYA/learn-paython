3e6f00ae614113974b6c202fa906f11feb4e9d0a
FOXAYA <omniasaged111@gmail.com>
2023-09-30 21:36:24 +0300

    updated

--- new_lang.py
+++ new_lang.py
@@ -98,6 +98,5 @@ name = "Osama"
 age = 38
 country = "Egypt"
 print("My Name %s , And My Age Is %d , And My Country Is %s" %(name,age,country)) #old_way
-print("My Name {} , And My Age Is {:d} , And My Country Is {}".format (name,age,country))#new_way
-# Needed Output Using f""
-# My Name Is Osama, And My Age Is 38, And My Country Is Egypt
+print("My Name {:s} , And My Age Is {:d} , And My Country Is {:s}".format (name,age,country))#new_way
+
